d0a8cb9deaa906821303a71e0e05888d88ec289d
wlee-a10 <wlee@a10networks.com>
2020-01-09 10:46:54 +0000

    Add script.

---
+++ auto_test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+version="1.0"
+
+srv_ip=$1
+srv_port=$2
+
+test_case=(\
+  "tc-hdr-conn"\
+)
+
+echo "++++++++++++++++++++"
+echo "+ AutoTest ver.$version +"
+echo -e "++++++++++++++++++++\n"
+echo "Start testing"
+echo "============================================================"
+for tc in ${test_case[@]}
+do
+    source $tc
+
+    rm -f result_$tc
+    
+    echo -e "\e[36m$tc\e[0m:"
+    echo "Failed Case(s)"
+    for idx in `seq 0 $((${#request[@]} - 1))`
+    do
+        echo -n "Case $idx: "  >> result_$tc
+	echo -ne "${request[$idx]}" | nc -w3 $srv_ip $srv_port | grep "${signature[$idx]}" > /dev/null && echo -e "Passed" >> result_$tc || (echo -e "Failed" >> result_$tc && echo -e "\e[31mCase $idx\e[0m" )
+ 
+	echo "Request:" >> result_$tc
+	echo -ne "${request[$idx]}" | sed -e "s/\t/(HTAB)/g" -e "s/^/\t|/"  >> result_$tc
+	echo "Response:" >> result_$tc
+	echo -ne "${request[$idx]}" | nc -w3 $srv_ip $srv_port | sed -e "s/\t/(HTAB)/g" -e "s/^/\t|/"  >> result_$tc
+	echo -ne "\nSignature: ${signature[$idx]}"  >> result_$tc
+        echo -e "\n============================================================"  >> result_$tc
+    done
+    echo -e "[Summary] Total: $(($idx+1)), \e[32mPassed\e[0m: `cat result_$tc | grep Passed | wc -l`, \e[31mFailed\e[0m: `cat result_$tc | grep Failed | wc -l`"
+    echo "============================================================"
+done
+
+echo "Test finished"
