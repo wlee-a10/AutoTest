80366b804c28a83001790c89bd7bdf294cae77a6
wlee-a10 <wlee@a10networks.com>
2020-01-14 08:19:40 +0000

    Version 3.0; The script would create a directory for a IP-port pair; Adopt new test case format;

--- auto_test.sh
+++ auto_test.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-version="2.0"
+version="3.0"
 
 srv_ip=$1
 srv_port=$2
 
 all=($@)
+
+nc_opt="-w1"
 
 if [ $# -gt 2 ]
 then
@@ -19,6 +21,10 @@ else
     exit 1
 fi
 
+dir="$srv_ip":"$srv_port"
+
+ls $dir > /dev/null || mkdir $dir
+
 echo "++++++++++++++++++++"
 echo "+ AutoTest ver.$version +"
 echo -e "++++++++++++++++++++\n"
@@ -28,23 +34,42 @@ for tc in ${test_case[@]}
 do
     source $tc
 
-    rm -f result_$tc
-    
+    result_file="$dir/result_$tc"
+    rm -f $result_file
+
     echo -e "\e[36m$tc\e[0m:"
     echo "Failed Case(s)"
     for idx in `seq 0 $((${#request[@]} - 1))`
     do
-        echo -n "Case $idx: "  >> result_$tc
-	echo -ne "${request[$idx]}" | nc -N $srv_ip $srv_port | grep "${signature[$idx]}" > /dev/null && echo -e "Passed" >> result_$tc || (echo -e "Failed" >> result_$tc && echo -e "\e[31mCase $idx\e[0m" )
- 
-	echo "Request:" >> result_$tc
-	echo -ne "${request[$idx]}" | sed -e "s/\t/(HTAB)/g" -e "s/^/\t|/"  >> result_$tc
-	echo "Response:" >> result_$tc
-	echo -ne "${request[$idx]}" | nc -N $srv_ip $srv_port | sed -e "s/\t/(HTAB)/g" -e "s/^/\t|/"  >> result_$tc
-	echo -ne "\nSignature: ${signature[$idx]}"  >> result_$tc
-        echo -e "\n============================================================"  >> result_$tc
+        echo -n "Case $idx: "  >> $result_file
+	    response="`echo -ne "${request[$idx]}" | nc $nc_opt $srv_ip $srv_port`"
+        valid=(${valid_pattern[$idx]})
+
+        passed=0
+
+        for i in `seq 0 $((${#valid[@]} - 1))`
+        do
+            echo -ne "$response" | grep "${pattern[${valid[$i]}]}" > /dev/null
+            if [ $? -eq 0 ]
+            then
+                passed=1
+                echo -e "Passed" >> $result_file && break
+            fi
+        done
+
+        if [ $passed -ne 1 ]
+        then
+            echo -e "Failed" >> $result_file && echo -e "\e[31mCase $idx\e[0m" 
+        fi
+
+	    echo "Request:" >> $result_file
+	    echo -ne "${request[$idx]}" | sed -e "s/\t/(HTAB)/g" -e "s/^/\t|/"  >> $result_file
+	    echo "Response:" >> $result_file
+	    echo -ne "${request[$idx]}" | nc $nc_opt $srv_ip $srv_port | sed -e "s/\t/(HTAB)/g" -e "s/^/\t|/"  >> $result_file
+	    echo -ne "\nSignature: ${signature[$idx]}"  >> $result_file
+        echo -e "\n============================================================"  >> $result_file
     done
-    echo -e "[Summary] Total: $(($idx+1)), \e[32mPassed\e[0m: `cat result_$tc | grep Passed | wc -l`, \e[31mFailed\e[0m: `cat result_$tc | grep Failed | wc -l`"
+    echo -e "[Summary] Total: $(($idx+1)), \e[32mPassed\e[0m: `cat $result_file | grep Passed | wc -l`, \e[31mFailed\e[0m: `cat $result_file | grep Failed | wc -l`"
     echo "============================================================"
 done
 
